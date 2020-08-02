19ae98fe406c56dc3a622bdbda68eb858d2c6fb1
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-03 00:34:21 +0300

    Dependency Injection Project installed

---
+++ DesignPatterns/Dependency Injection/PersonDataReader.Service/ServiceReader.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using PeopleViewer.Common;
+using System.Collections.Generic;
+using System.Net;
+
+namespace PersonDataReader.Service
+{
+    public class ServiceReader
+    {
+        private readonly WebClient client = new WebClient();
+        private const string baseUri = "http://localhost:9874/api/people";
+
+        public IEnumerable<Person> GetPeople()
+        {
+            var result = client.DownloadString(baseUri);
+            var people = JsonConvert.DeserializeObject<IEnumerable<Person>>(result);
+            return people;
+        }
+
+        public Person GetPerson(int id)
+        {
+            var result = client.DownloadString($"{baseUri}/{id}");
+            var person = JsonConvert.DeserializeObject<Person>(result);
+            return person;
+        }
+    }
+}
