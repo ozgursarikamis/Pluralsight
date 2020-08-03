19f81e16553f16fc1e72a043583e66b64bc81bab
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-03 19:08:22 +0300

    Ninject Configuration

---
+++ DesignPatterns/Dependency Injection/PeopleViewer.Ninject/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using Ninject;
+using PeopleViewer.Common;
+using PersonDataReader.CSV;
+using PersonDataReader.Service;
+
+namespace PeopleViewer.Ninject
+{
+    public partial class App : Application
+    {
+        private readonly IKernel Container = new StandardKernel();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+            
+            Current.MainWindow.Title = "With Dependency Injection - Ninject";
+            Current.MainWindow.Show();
+        }
+
+        private void ConfigureContainer()
+        {
+            Container.Bind<IPersonReader>().To<ServiceReader>();
+        }
+
+        private void ComposeObjects()
+        {
+            Current.MainWindow = Container.Get<PeopleViewerWindow>();
+        }
+    }
+}
