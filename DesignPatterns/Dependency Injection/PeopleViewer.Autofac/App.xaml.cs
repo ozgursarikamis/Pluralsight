e58825cf0bdb067829e43dfe1bcc118ae062d1e2
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-03 19:19:26 +0300

    Autofac - Configuration

---
+++ DesignPatterns/Dependency Injection/PeopleViewer.Autofac/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using Autofac;
+using Autofac.Features.ResolveAnything;
+using PeopleViewer.Common;
+using PersonDataReader.Service;
+
+namespace PeopleViewer.Autofac
+{
+    public partial class App : Application
+    {
+        private static IContainer Container;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+            Application.Current.MainWindow.Title = "With Dependency Injection - Autofac";
+            Application.Current.MainWindow.Show();
+        }
+
+        private static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ServiceReader>().As<IPersonReader>()
+                .SingleInstance();
+
+            builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+
+            Container = builder.Build();
+        }
+
+        private static void ComposeObjects()
+        {
+            Current.MainWindow = Container.Resolve<PeopleViewerWindow>();
+        }
+    }
+}
