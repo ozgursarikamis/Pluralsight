161eb3f2ac8f57d42c111a9fca15d4550a4a3c77
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-04 19:06:59 +0300

    convert to a not-nullable reference type

--- Language/Working With Nulls/03 Null Basics with CS8/NullBasics/Program.cs
+++ Language/Working With Nulls/03 Null Basics with CS8/NullBasics/Program.cs
@@ -6,7 +6,10 @@ namespace NullBasics
     {
         private static void Main()
         {
+#nullable disable  // convert to a not-nullable reference type
             string message = null;
+#nullable enable
+
             Console.WriteLine(message); // string is a reference type, so it can be null.
             Console.WriteLine("Press Enter to end!");
             Console.ReadLine();
