8b3b72e0b72c0fba6cd3a9f2e0a59e2e856db8ce
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-05 02:16:47 +0300

    Mapping object to object with AutoMapper

--- Web/1 Building Restful APIs/CourseLibrary.API/Controllers/AuthorsController.cs
+++ Web/1 Building Restful APIs/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using AutoMapper;
 using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
@@ -16,28 +17,24 @@ namespace CourseLibrary.API.Controllers
     public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
-        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        private readonly IMapper _mapper;
+
+        public AuthorsController(
+            ICourseLibraryRepository courseLibraryRepository,
+            IMapper mapper
+            )
         {
             _courseLibraryRepository = courseLibraryRepository ??
                                        throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ??
+                      throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
         {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
-            var authors = new List<AuthorDto>();
-            foreach (var author in authorsFromRepo)
-            {
-                authors.Add(new AuthorDto
-                {
-                    Id = author.Id,
-                    Name = $"{author.FirstName} {author.LastName}",
-                    MainCategory = author.MainCategory,
-                    Age = author.DateOfBirth.GetCurrentAge()
-                });
-            }
-            return Ok(authors);
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
         }
 
         [HttpGet("{authorId:guid}")]
