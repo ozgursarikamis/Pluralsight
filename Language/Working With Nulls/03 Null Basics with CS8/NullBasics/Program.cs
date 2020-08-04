d75b47b417170d5e5f0108428c8ef73d520cf97f
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-04 18:59:44 +0300

    Null Basics with C# 8 installed

---
+++ Language/Working With Nulls/03 Null Basics with CS8/NullBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NullBasics
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
