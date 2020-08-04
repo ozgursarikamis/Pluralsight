fc4f0611763cdebb6c02beea9f468b3a630190e7
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-05 02:05:26 +0300

    use DTO

--- Web/1 Building Restful APIs/CourseLibrary.API/Controllers/AuthorsController.cs
+++ Web/1 Building Restful APIs/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,10 +23,21 @@ namespace CourseLibrary.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAuthors()
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
         {
             var authorsFromRepo = _courseLibraryRepository.GetAuthors();
-            return Ok(authorsFromRepo);
+            var authors = new List<AuthorDto>();
+            foreach (var author in authorsFromRepo)
+            {
+                authors.Add(new AuthorDto
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName} {author.LastName}",
+                    MainCategory = author.MainCategory,
+                    Age = author.DateOfBirth.GetCurrentAge()
+                });
+            }
+            return Ok(authors);
         }
 
         [HttpGet("{authorId:guid}")]
