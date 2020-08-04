aca29fd585848c070c05afc1904152b1a7d2bc0c
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-05 00:09:44 +0300

    building restful api project created

--- Web/Advanced Restful Concerns/CourseLibrary.API/Migrations/20200804211650_InitialMigration.Designer.cs
+++ Web/Advanced Restful Concerns/CourseLibrary.API/Migrations/20200804211650_InitialMigration.Designer.cs
@@ -4,13 +4,16 @@ using CourseLibrary2.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CourseLibrary2.API.Migrations
 {
     [DbContext(typeof(CourseLibraryContext))]
-    partial class CourseLibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20200804211650_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
