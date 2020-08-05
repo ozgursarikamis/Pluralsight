aa9d8ff707b2427898788af81ae254ba8e059f1b
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-06 01:46:22 +0300

    IValidatableObject

--- Web/1 Building Restful APIs/CourseLibrary.API/Models/CourseDto.cs
+++ Web/1 Building Restful APIs/CourseLibrary.API/Models/CourseDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
@@ -11,11 +12,22 @@ namespace CourseLibrary.API.Models
         public Guid AuthorId { get; set; }
     }
 
-    public class CourseForCreationDto
+    public class CourseForCreationDto : IValidatableObject
     {
         [Required, MaxLength(100)]
         public string Title { get; set; }
         [MaxLength(1500)]
-        public string Description { get; set; } 
+        public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult(
+                    "The provided description should be different from the title",
+                    new[] {"CourseCreationForDto" }
+                );
+            }
+        }
     }
 }
