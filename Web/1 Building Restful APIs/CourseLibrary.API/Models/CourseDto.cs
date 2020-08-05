6b360651e353c5a5a534f0c8e8120bfa3086f0b5
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-06 02:36:46 +0300

    duplicate codes for property validation

--- Web/1 Building Restful APIs/CourseLibrary.API/Models/CourseDto.cs
+++ Web/1 Building Restful APIs/CourseLibrary.API/Models/CourseDto.cs
@@ -17,7 +17,7 @@ namespace CourseLibrary.API.Models
         (ErrorMessage = "Title must be different from the description.")]
     public class CourseForCreationDto // : IValidatableObject
     {
-        [Required(ErrorMessage = "You should fill out a title")]
+        [Required(ErrorMessage = "You should fill out a title.")]
         [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters")]
         public string Title { get; set; }
         [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters")]
@@ -35,9 +35,16 @@ namespace CourseLibrary.API.Models
         //}
     }
 
+    [CourseTitleMustBeDifferentFromDescription
+        (ErrorMessage = "Title must be different from the description.")]
     public class CourseForUpdateDto
     {
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "You should fill out a description.")]
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters")]
         public string Description { get; set; }
     }
 }
