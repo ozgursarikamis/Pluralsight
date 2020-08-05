d066132d7d319d72b85f6da7351fa2ce37e5fd97
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-06 01:38:14 +0300

    annotate for validation

--- Web/1 Building Restful APIs/CourseLibrary.API/Models/CourseDto.cs
+++ Web/1 Building Restful APIs/CourseLibrary.API/Models/CourseDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseLibrary.API.Models
 {
@@ -12,7 +13,9 @@ namespace CourseLibrary.API.Models
 
     public class CourseForCreationDto
     {
+        [Required, MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(1500)]
         public string Description { get; set; } 
     }
 }
