e118fbb17b78842d1f8d55a1bc803220b4c3fd0d
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-09 03:10:48 +0300

    adding cache headers to the response

--- Web/1 Building Restful APIs/CourseLibrary.API/Controllers/CoursesController.cs
+++ Web/1 Building Restful APIs/CourseLibrary.API/Controllers/CoursesController.cs
@@ -39,6 +39,7 @@ namespace CourseLibrary.API.Controllers
         }
 
         [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
+        [ResponseCache(Duration = 120)]
         public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
         {
             if (!_courseLibraryRepository.AuthorExists(authorId))
