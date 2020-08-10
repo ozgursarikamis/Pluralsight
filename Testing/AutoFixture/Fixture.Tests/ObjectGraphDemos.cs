dee02f1fb41f9905892298d6520c6e7293f900d8
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-10 14:18:22 +0300

    creating complex objects

---
+++ Testing/AutoFixture/Fixture.Tests/ObjectGraphDemos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoFixture;
+using Xunit;
+
+namespace Fixture.Tests
+{
+    public class ObjectGraphDemos
+    {
+        [Fact]
+        public void ManualCreation()
+        {
+            Customer customer = new Customer
+            {
+                CustomerName = "Amrit"
+            };
+
+            Order order = new Order(customer)
+            {
+                Id = 412,
+                OrderDate = DateTime.Now,
+                Items = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        ProductName = "Rubber ducks",
+                        Quantity = 2
+                    }
+                }
+            };
+        }
+
+        [Fact]
+        public void AutoCreation()
+        {
+            var fixture = new AutoFixture.Fixture();
+            Order order = fixture.Create<Order>();
+
+        }
+    }
+}
