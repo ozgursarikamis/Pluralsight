87af36b827d3ab5fbf216f0df4eff3516a6baae3
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-10 12:15:41 +0300

    enums and guids

---
+++ Testing/AutoFixture/Fixture.Tests/GuidEnumDemos.cs
@@ -0,0 +1,39 @@
+using System;
+using AutoFixture; 
+using Xunit;
+
+namespace Fixture.Tests
+{
+    public class GuidEnumDemos
+    {
+        [Fact]
+        public void Guid()
+        {
+            var fixture = new AutoFixture.Fixture();
+            var sut = new EmailMessage(
+                fixture.Create<string>(), 
+                fixture.Create<string>(), 
+                fixture.Create<bool>());
+
+            sut.Id = fixture.Create<Guid>();
+
+
+        }
+
+        [Fact]
+        public void Enums()
+        {
+            var fixture = new AutoFixture.Fixture();
+
+            var sut = new EmailMessage(
+                fixture.Create<string>(),
+                fixture.Create<string>(),
+                fixture.Create<bool>());
+
+            sut.Id = fixture.Create<Guid>();
+            sut.MessageType = fixture.Create<EmailMessageType>();
+
+
+        }
+    }
+}
