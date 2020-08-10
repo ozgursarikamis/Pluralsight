07d1faf19fbbc73ac5710c243100a258134f8154
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-11 01:20:18 +0300

    Change setting value, the app does not need recompiling

--- Web/Configuration and Options/src/TennisBookings.Web/Pages/Index.cshtml.cs
+++ Web/Configuration and Options/src/TennisBookings.Web/Pages/Index.cshtml.cs
@@ -7,13 +7,15 @@ namespace TennisBookings.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IWeatherForecaster _weatherForecaster;
         private readonly IGreetingService _greetingService;
         private readonly IConfiguration _configuration;
 
-        public IndexModel(IGreetingService greetingService, IConfiguration configuration)
+        public IndexModel(IGreetingService greetingService, IConfiguration configuration, IWeatherForecaster weatherForecaster)
         {
             _greetingService = greetingService;
             _configuration = configuration;
+            _weatherForecaster = weatherForecaster;
         }
 
         public string Greeting { get; private set; }
@@ -28,6 +30,36 @@ namespace TennisBookings.Web.Pages
             if (homePageFeatures.GetValue<bool>("EnableGreeting"))
             {
                 Greeting = _greetingService.GetRandomGreeting();
+            }
+
+            ShowWeatherForecast = homePageFeatures.GetValue<bool>("EnableWeatherForecast");
+            if (ShowWeatherForecast)
+            {
+                var title = homePageFeatures["ForecastSectionTitle"];
+                ForecastSectionTitle = string.IsNullOrEmpty(title) ? "How is the weather" : title;
+
+                var currentWeather = await _weatherForecaster.GetCurrentWeatherAsync();
+                if (currentWeather != null)
+                {
+                    switch (currentWeather.Description)
+                    {
+                        case "Sun":
+                            WeatherDescription = "sunny";
+                            break;
+                        case "Cloud":
+                            WeatherDescription = "cloudy";
+                            break;
+                        case "Rain":
+                            WeatherDescription = "rainy";
+                            break;
+                        case "Snow":
+                            WeatherDescription = "Snowy";
+                            break;
+                        default:
+                            WeatherDescription = "";
+                            break;
+                    }
+                }
             }
         }
     }
