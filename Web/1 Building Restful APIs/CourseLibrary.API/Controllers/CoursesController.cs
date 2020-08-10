c45df5860f8c8bd3605087d058543f6fe94154f9
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-10 10:14:16 +0300

    resource-level cache header configuration

--- Web/1 Building Restful APIs/CourseLibrary.API/Controllers/CoursesController.cs
+++ Web/1 Building Restful APIs/CourseLibrary.API/Controllers/CoursesController.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using CourseLibrary.API.Entities;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
+using Marvin.Cache.Headers;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
@@ -40,7 +41,9 @@ namespace CourseLibrary.API.Controllers
         }
 
         [HttpGet("{courseId}", Name = "GetCourseForAuthor")]
-        [ResponseCache(Duration = 120, Location = ResponseCacheLocation.Any)]
+        // [ResponseCache(Duration = 120, Location = ResponseCacheLocation.Any)]
+        [HttpCacheExpiration(CacheLocation = CacheLocation.Public, MaxAge = 1000)]
+        [HttpCacheValidation(MustRevalidate = false)]
         public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
         {
             if (!_courseLibraryRepository.AuthorExists(authorId))
