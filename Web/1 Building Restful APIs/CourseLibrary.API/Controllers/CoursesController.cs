49a7f558e7e74feaa2900d628bf947d50dffdaeb
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-10 10:05:53 +0300

    adding support for generating ETags

--- Web/1 Building Restful APIs/CourseLibrary.API/Controllers/CoursesController.cs
+++ Web/1 Building Restful APIs/CourseLibrary.API/Controllers/CoursesController.cs
@@ -15,7 +15,7 @@ namespace CourseLibrary.API.Controllers
 {
     [ApiController]
     [Route("api/authors/{authorId}/courses")]
-    [ResponseCache(CacheProfileName = "240SecondsCacheProfile")]
+    // [ResponseCache(CacheProfileName = "240SecondsCacheProfile")]
     public class CoursesController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
--- Web/1 Building Restful APIs/CourseLibrary.API/Startup.cs
+++ Web/1 Building Restful APIs/CourseLibrary.API/Startup.cs
@@ -28,6 +28,8 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpCacheHeaders();
+
             services.AddResponseCaching();
 
             services.AddControllers(setUpAction =>
@@ -75,12 +77,10 @@ namespace CourseLibrary.API
             services.Configure<MvcOptions>(config =>
             {
                 var newtonsoftJsonOutputFormatter = config.OutputFormatters
-                    .OfType<NewtonsoftJsonOutputFormatter>()?.FirstOrDefault();
+                    .OfType<NewtonsoftJsonOutputFormatter>()
+                    .FirstOrDefault();
 
-                if (newtonsoftJsonOutputFormatter != null)
-                {
-                    newtonsoftJsonOutputFormatter.SupportedMediaTypes.Add("application/vnd.marvin.hateoas+json");
-                }
+                newtonsoftJsonOutputFormatter?.SupportedMediaTypes.Add("application/vnd.marvin.hateoas+json");
             });
 
             // register PropertyMappingService
@@ -117,6 +117,8 @@ namespace CourseLibrary.API
             }
 
             app.UseResponseCaching();
+
+            app.UseHttpCacheHeaders();
 
             app.UseRouting();
 
