787e0318f8a0bdca6675f0cdf324cb09c6fffb8c
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-10 21:59:03 +0300

    custom generators

--- Testing/AutoFixture/Fixture.Tests/CustomizationDemos.cs
+++ Testing/AutoFixture/Fixture.Tests/CustomizationDemos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AutoFixture;
+using Fixture.Tests.SpecimenBuilders;
 using Xunit;
 
 namespace Fixture.Tests
@@ -16,6 +17,16 @@ namespace Fixture.Tests
 
             var date1 = fixture.Create<DateTime>();
             var date2 = fixture.Create<DateTime>();
+        }
+
+        [Fact]
+        public void CustomizationPipeline()
+        {
+            var fixture = new AutoFixture.Fixture();
+            fixture.Customizations.Add(new AirportCodeStringPropertyGenerator());
+
+            var flight = fixture.Create<FlightDetails>();
+            var airport = fixture.Create<Airport>();
         }
     }
 }
---
+++ Testing/AutoFixture/Fixture.Tests/SpecimenBuilders/AirportCodeStringPropertyGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using AutoFixture.Kernel;
+
+namespace Fixture.Tests.SpecimenBuilders
+{
+    public class AirportCodeStringPropertyGenerator : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            // see if we are trying to create a value for a property:
+            var propertyInfo = request as PropertyInfo;
+            if (propertyInfo is null)
+            {
+                // this specimen builder does not apply to current request
+
+                // Null is a valid specimen so retun NoSpecimen
+                return new NoSpecimen(); 
+            }
+
+            // now er know we're dealing with a property 
+            // are we creating a value for an airport code?
+
+            var isAirportCodeProperty = propertyInfo.Name.Contains("AirportCode");
+            var isStringProperty = propertyInfo.PropertyType == typeof(string);
+
+            if (isAirportCodeProperty && isStringProperty)
+            {
+                return RandomAirportCode();
+            }
+
+            return new NoSpecimen();
+        }
+
+        private static string RandomAirportCode() => 
+            DateTime.Now.Ticks % 2 == 0 ? "LHR" : "PER";
+    }
+}
