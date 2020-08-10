2936eb802ae8a58a37d8298104a9d22c5e9aa99a
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-10 21:32:01 +0300

    Customizations

---
+++ Testing/AutoFixture/Fixture.Tests/CustomizationDemos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AutoFixture;
+using Xunit;
+
+namespace Fixture.Tests
+{
+    public class CustomizationDemos
+    {
+        [Fact]
+        public void DateTimeCustomization()
+        {
+            var fixture = new AutoFixture.Fixture();
+
+            // fixture.Customize(new CurrentDateTimeCustomization());
+            fixture.Customizations.Add(new CurrentDateTimeGenerator());
+
+            var date1 = fixture.Create<DateTime>();
+            var date2 = fixture.Create<DateTime>();
+        }
+    }
+}
