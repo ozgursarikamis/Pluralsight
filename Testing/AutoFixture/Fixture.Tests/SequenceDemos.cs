8c6b79ec8eba18386e45471a71eb19237d047e49
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-10 12:25:41 +0300

    generating multiple values at once

---
+++ Testing/AutoFixture/Fixture.Tests/SequenceDemos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Net.Mail;
+using AutoFixture;
+using Xunit;
+
+namespace Fixture.Tests
+{
+    public class SequenceDemos
+    {
+        [Fact]
+        public void SequenceOfStrings()
+        {
+            var fixture = new AutoFixture.Fixture();
+
+            IEnumerable<string> messages = fixture.CreateMany<string>(6);
+
+        }
+    }
+}
