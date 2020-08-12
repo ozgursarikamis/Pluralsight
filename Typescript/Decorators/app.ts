1892e37f10b7ac6fef8e4ac7f12cbd48c9248fb6
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-12 03:06:16 +0300

    generate first Decorator with express

--- Typescript/Decorators/app.ts
+++ Typescript/Decorators/app.ts
@@ -1,46 +1,23 @@
-function logClass(message: string): ClassDecorator {
-    console.log(`${message} evaluated`);
-    return function (constructor: Function): void {
-        console.log(`${message} called`);
+import APIServer from "./APIServer";
+import { Request, Response } from "express";
+
+const server = new APIServer();
+
+class APIRoutes {
+    @route("get", "/")
+    public indexRoute(req: Request, res: Response) {
+        return {
+            "Hello": "World"
+        }
     }
 }
 
-function logProperty(message: string): PropertyDecorator {
-    console.log(`${message} evaluated`);
-    return function (target: Object, propertyKey: string): void {
-        console.log(`${message} called`);
+function route(method:string, path: string): MethodDecorator {
+    return function (target:any, propertyKey: string, descriptor: PropertyDescriptor) {
+        server.app[method](path, (req: Request, res: Response) => {
+            res.status(200).json(descriptor.value(req, res));
+        })
     }
 }
 
-function logMethod(message: string): MethodDecorator {
-    console.log(`${message} evaluated`);
-    return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor): void {
-        console.log(`${message} called`);
-    }
-}
-
-function logParameter(message: string): ParameterDecorator {
-    console.log(`${message} evaluated`);
-    return function (target: Object, propertyKey: string, parameterIndex: number): void {
-        console.log(`${message} called`);
-    }
-}
-
-@logClass("Class Decorator")
-class Person {
-    private _directReports: Person[];
-
-    @logProperty("Property Decorator")
-    public emailAddress: string;
-
-    constructor(public firstName: string, public lastName: string) {
-        this._directReports = [];
-    }
-
-    @logMethod("Method Decorator")
-    public addDirectReport(@logParameter("Parameter Decorator") person: Person) {
-        this._directReports.push(person);
-    }
-}
-
-const person = new Person("David", "Tucker");
+server.start();
