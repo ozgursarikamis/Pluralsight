6f61634917136d7f04f586323b6f389a2823ce46
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-08-12 17:39:43 +0300

    moduleResolution

--- Typescript/Modules/b.ts
+++ Typescript/Modules/b.ts
@@ -1,7 +1,8 @@
-import { isOdd } from "./utils";
+import { isOdd, reverse } from "./utils";
 import { aString } from "./a";
 
 console.log(aString);
 
 const isOdd_ = isOdd(21);
 console.log('isOdd_ :>> ', isOdd_);
+console.log('reverse :>> ', reverse('test'));
